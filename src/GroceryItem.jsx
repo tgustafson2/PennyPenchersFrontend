57bdd6998ce58538a46a9008695b481a69eac6d1
Caleb C <wolfertamer@gmail.com>
2024-04-21 12:45:21 -0700

    added colors to the price check page (partly hardcoded)

--- src/GroceryItem.jsx
+++ src/GroceryItem.jsx
@@ -6,10 +6,19 @@ function GroceryItem(props) {
 const [item, setItem] = useState(props.item);
 const [inCart, setInCart] = useState(false);
 
+let color;
+switch(item.ChainName) {
+  case 'Ralphs': color="tc-red-text"; break;
+  case 'Trader Joes': color="tc-yellow-text"; break;
+  case 'Vons': color="tc-blue-text"; break;
+  default: color="tc-green-text";
+}
+
+
   return (
     <>
       <li class="tc-grid-container">
-        <div class="left-align">{item.ItemName}</div>
+        <div class={"left-align "+color}>{item.ItemName}</div>
         <div class="w3-small right-text">${item.ItemCost} @</div>
         <div class="w3-small left-align">{item.ChainName}</div>
       </li>
